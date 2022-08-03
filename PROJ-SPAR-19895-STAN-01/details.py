96a8a3ef093841d6584d6af2ec36b081332bb113
Oom Lout <aaron@oomlout.com>
2022-08-03 17:49:58 +0100

    Added new designs.

---
+++ PROJ-SPAR-19895-STAN-01/details.py
@@ -0,0 +1,19 @@
+###### OOMP FILE  ######
+
+import OOMP
+import OOMPtags
+
+newPart = OOMP.oompItem()
+
+newPart.addTag("hexID", "PRS19895")
+newPart.addTag("oompType", "PROJ")
+newPart.addTag("oompSize", "SPAR")
+newPart.addTag("oompColor", "19895")
+newPart.addTag("oompDesc", "STAN")
+newPart.addTag("oompIndex", "01")
+newPart.addTag("oompName", "SparkFun Qwiic 9DoF-ISM330DHCX-MMC5983MA")
+
+newPart.addTag("sources", "All source files from https://github.com/sparkfun/SparkFun_Qwiic_9DoF-ISM330DHCX-MMC5983MA (source licence details in srcLicense.md)")
+newPart.addTag("linkBuyPage", "https://www.sparkfun.com/products/19895")
+
+OOMP.parts.append(newPart)
---
+++ PROJ-SPAR-19921-STAN-01/details.py
@@ -0,0 +1,19 @@
+###### OOMP FILE  ######
+
+import OOMP
+import OOMPtags
+
+newPart = OOMP.oompItem()
+
+newPart.addTag("hexID", "PRS19921")
+newPart.addTag("oompType", "PROJ")
+newPart.addTag("oompSize", "SPAR")
+newPart.addTag("oompColor", "19921")
+newPart.addTag("oompDesc", "STAN")
+newPart.addTag("oompIndex", "01")
+newPart.addTag("oompName", "SparkFun Qwiic Magnetometer-MMC5983MA")
+
+newPart.addTag("sources", "All source files from https://github.com/sparkfun/SparkFun_Qwiic_Magnetometer-MMC5983MA (source licence details in srcLicense.md)")
+newPart.addTag("linkBuyPage", "https://www.sparkfun.com/products/19921")
+
+OOMP.parts.append(newPart)
