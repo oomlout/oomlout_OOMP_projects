71134ae0446446baff89aec1c9dd1522a0970ccd
Oom Lout <aaron@oomlout.com>
2022-09-19 19:22:11 +0100

    Attempt at part matching.

--- PROJ-ADAF-1755-STAN-01/detailsPartsOomp.py
+++ PROJ-ADAF-1755-STAN-01/detailsPartsOomp.py
@@ -1,5 +1,5 @@
 import OOMP
 newPart = OOMP.getPartByID("PROJ-ADAF-1755-STAN-01")
 
-newPart.addTag('oompParts','LED1,UNMATCHED-UNMATCHED-UNMATCHED-UNMATCHED-UNMATCHED')
-newPart.addTag('oompParts','R1,UNMATCHED-UNMATCHED-UNMATCHED-UNMATCHED-UNMATCHED')
+newPart.addTag('oompParts','LED1,LEDS-1206-G-STAN-01')
+newPart.addTag('oompParts','R1,RESE-0603-X-UNMATCHED-01')
